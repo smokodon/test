52061f8df0ab1c0e603b73c677ad6731ac7c68a9
Konrad Wrzos <konrad@travis-ci.org>
2022-02-08 14:56:09 +0100

    test

--- script.sh
+++ script.sh
@@ -5,7 +5,6 @@ set -o errexit
 function main() {
  touch /etc/default/grub
  echo "GRUB_CMDLINE_LINUX=systemd.unified_cgroup_hierarchy=1" >> /etc/default/grub
- ls /etc/default/grub
-}
+ }
 
 main "$@"
