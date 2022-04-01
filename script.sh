2155c377e0995671e3c9283fda1e819c4793337b
Konrad Wrzos <konrad@travis-ci.org>
2022-04-01 12:57:58 +0200

    test

--- script.sh
+++ script.sh
@@ -4,15 +4,14 @@ set -o errexit
 set -o xtrace
 
 main() {
-
+    
   local JAVA_VERSION
   JAVA_VERSION="17"
   sudo apt-get update -yqq
   PACKAGE="temurin-${JAVA_VERSION}-jdk"
 
     wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
-    sudo add-apt-repository --yes https://packages.adoptium.net/artifactory/deb
-    #echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+    echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
     sudo apt-get update -yqq
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s "$PACKAGE"*
