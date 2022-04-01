1a1750e8045189cfd48934b7c2447a07c3e4f618
Konrad Wrzos <konrad@travis-ci.org>
2022-04-01 13:11:49 +0200

    test

--- script.sh
+++ script.sh
@@ -9,11 +9,11 @@ main() {
   JAVA_VERSION="17"
   sudo apt-get update -yqq
   PACKAGE="temurin-${JAVA_VERSION}-jdk"
-
     wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
     echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
     sudo apt-get update -yqq
-    sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
+    sudo apt-get install "$PACKAGE"
+    #sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s "$PACKAGE"*
 }
 
