88bc0a1b41a6a0c25befbaebf622b1a6a8f8cb65
Konrad Wrzos <konrad@travis-ci.org>
2022-04-01 11:28:00 +0200

    test

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
   #!/usr/bin/env bash
   
-  main() {
+main() {
   local JAVA_VERSION
   JAVA_VERSION="17"
   sudo apt-get update -yqq
   PACKAGE="temurin-${JAVA_VERSION}-jdk"
   if ! dpkg -s "$PACKAGE" >/dev/null 2>&1; then
     if dpkg-query -l temurin* >/dev/null 2>&1; then
-      dpkg-query -l temurin* | grep temurin | awk '{print $2}' | xargs sudo dpkg -P
+       dpkg-query -l temurin* | grep temurin | awk '{print $2}' | xargs sudo dpkg -P
     fi
     wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
     echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
