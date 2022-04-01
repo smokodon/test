74227a357acd9c464c779649e6186a67a88da3d1
Konrad Wrzos <konrad@travis-ci.org>
2022-04-01 16:26:28 +0200

    test

--- script.sh
+++ script.sh
@@ -14,6 +14,7 @@ main() {
     sudo apt-get update -yqq
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s "$PACKAGE"*
+    sudo update-java-alternatives -l
     #export JAVA_HOME="/usr/lib/jvm/$PACKAGE"
 }
 
