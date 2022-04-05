1af844a3ef3edf193b6db634e946b39f34ebf151
Konrad Wrzos <konrad@travis-ci.org>
2022-04-05 14:30:08 +0200

    test

--- script.sh
+++ script.sh
@@ -12,6 +12,8 @@ main() {
     wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
     echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
     sudo apt-get update -yqq
+    travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
+    travis_cmd "export JAVA_HOME=~/$PACKAGE"
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s /usr/lib/jvm/"$PACKAGE"* --verbose
     #export JAVA_HOME="/usr/lib/jvm/$PACKAGE"
