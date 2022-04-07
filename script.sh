6b83fa557afeb302362fd42ade7747f705987b9a
Konrad Wrzos <konrad@travis-ci.org>
2022-04-07 09:59:55 +0200

    test

--- script.sh
+++ script.sh
@@ -12,9 +12,9 @@ main() {
     wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
     echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
     sudo apt-get update -yqq
-    export JAVA_HOME="/usr/bin/java"
+    #export JAVA_HOME="/usr/bin/java"
     # shellcheck disable=SC2016
-    export PATH="$JAVA_HOME/bin:$PATH"
+    #export PATH="$JAVA_HOME/bin:$PATH"
     sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
     sudo update-java-alternatives -s /usr/lib/jvm/"$PACKAGE"* --verbose
     echo $JAVA_HOME
