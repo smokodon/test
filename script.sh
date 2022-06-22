cde3548cd7e956d3c298b979d6034fbc33abe619
Konrad Wrzos <konrad@travis-ci.org>
2022-06-22 11:04:59 +0200

    testing

--- script.sh
+++ script.sh
@@ -4,22 +4,19 @@ set -o errexit
 set -o xtrace
 
 main() {
-    
+
   local JAVA_VERSION
-  JAVA_VERSION="17"
+  JAVA_VERSION=13
   sudo apt-get update -yqq
-  PACKAGE="temurin-${JAVA_VERSION}-jdk"
-    wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
-    echo "deb [signed-by=/usr/share/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
-    sudo apt-get update -yqq
-    #export JAVA_HOME="/usr/bin/java"
-    # shellcheck disable=SC2016
-    #export PATH="$JAVA_HOME/bin:$PATH"
-    sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
-    sudo update-java-alternatives -s /usr/lib/jvm/"$PACKAGE"* --verbose
-    whereis java
-    #echo $JAVA_HOME
-    #java -version
+  PACKAGE="bellsoft-java${JAVA_VERSION}"
+  wget -qO - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -
+  sudo add-apt-repository  'deb [arch='$TRAVIS_CPU_ARCH'] https://apt.bell-sw.com/ stable main'
+  sudo apt-get update -yqq
+  sudo apt-get -yqq --no-install-suggests --no-install-recommends install "$PACKAGE" || true
+  travis_cmd "export JAVA_HOME=/usr/lib/jvm/bellsoft-java${JAVA_VERSION}-${TRAVIS_CPU_ARCH}" --echo
+  travis_cmd 'export PATH="$JAVA_HOME/bin:$PATH"' --echo
+  sudo update-java-alternatives -s "$PACKAGE"*
+
 }
 
 main "$@"
