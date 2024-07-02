fc51fb4a2bcd5cb4c6214c3b0bad4ead510a7128
Konrad Wrzos <konrad@travis-ci.org>
2024-07-02 15:41:53 +0200

    semeru

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -12,11 +12,14 @@ wget "$JDK_URL" -O jdk.tar.gz
 # Extract the tarball
 tar xzf jdk.tar.gz -C "$INSTALL_DIR"
 
+export JAVA_HOME=$INSTALL_DIR/jdk
+export PATH=$PATH:$JAVA_HOME/bin
+
 # Set JAVA_HOME
 echo "export JAVA_HOME=$INSTALL_DIR/jdk" >> ~/.bashrc
 
 # Add JDK bin directory to PATH
-echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
+echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.bashrc
 
 # Reload the shell
 source ~/.bashrc
