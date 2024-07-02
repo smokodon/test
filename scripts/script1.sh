e4938b10c5b54b4eb0ef19c03105aa00a3988111
Konrad Wrzos <konrad@travis-ci.org>
2024-07-02 16:06:02 +0200

    semeru

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -4,7 +4,7 @@
 JDK_URL="https://github.com/ibmruntimes/semeru21-binaries/releases/download/jdk-21.0.3%2B9_openj9-0.44.0/ibm-semeru-open-jdk_x64_linux_21.0.3_9_openj9-0.44.0.tar.gz"
 
 # Define the installation directory
-INSTALL_DIR="/opt/jdk-21.0.3+9/bin"
+INSTALL_DIR="/opt"
 
 # Download the JDK tarball
 wget -q "$JDK_URL" -O jdk.tar.gz
@@ -14,7 +14,7 @@ tar xzf jdk.tar.gz -C "$INSTALL_DIR"
 
 ls -al $INSTALL_DIR
 
-export JAVA_HOME=$(cd "${INSTALL_DIR}"; pwd)
+export JAVA_HOME=$INSTALL_DIR/jdk-21.0.3+9/bin
 export PATH=$JAVA_HOME/bin:$PATH
 
 echo $JAVA_HOME
