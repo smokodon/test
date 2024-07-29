d4ea4523e65b8a2828ec767ec161e386333ea72d
Konrad Wrzos <konrad@travis-ci.org>
2024-07-29 09:44:50 +0200

    focal

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,74 +1,56 @@
 #!/bin/bash
 
-# # Set the URL for the JDK tar.gz archive
-# JDK_URL="https://github.com/ibmruntimes/semeru21-binaries/releases/download/jdk-21.0.3%2B9_openj9-0.44.0/ibm-semeru-open-jdk_x64_linux_21.0.3_9_openj9-0.44.0.tar.gz"
-
-# # Define the installation directory
-# INSTALL_DIR="/usr/lib/jvm"
-
-# # Download the JDK tarball
-# wget -q "$JDK_URL" -O jdk.tar.gz
-
-# # Extract the tarball
-# tar xzf jdk.tar.gz -C "$INSTALL_DIR"
-
-# export JAVA_HOME=$INSTALL_DIR/jdk-21.0.3+9
-# export PATH=$JAVA_HOME/bin:$PATH
-
-# # Reload the shell
-# source ~/.bashrc
-
-# # Verify Java version
-# java -version
-
-#local JAVA_VERSION
-  JAVA_VERSION="18"
-
-  if [[ "${TRAVIS_CPU_ARCH}" == "arm64" ]]; then
-    TRAVIS_CPU_ARCH="aarch64";
-  elif [[ "${TRAVIS_CPU_ARCH}" == "amd64" ]]; then
-    TRAVIS_CPU_ARCH="x64";
-  fi
-
-  case "${JAVA_VERSION}" in
-    8) JDK_URL="https://github.com/ibmruntimes/semeru8-binaries/releases/download/jdk8u412-b08_openj9-0.44.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_8u412b08_openj9-0.44.0.tar.gz" ;; 
-    11) JDK_URL="https://github.com/ibmruntimes/semeru11-binaries/releases/download/jdk-11.0.23%2B9_openj9-0.44.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_11.0.23_9_openj9-0.44.0.tar.gz" ;;
-    16) JDK_URL="https://github.com/ibmruntimes/semeru16-binaries/releases/download/jdk-16.0.2%2B7_openj9-0.27.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_16.0.2_7_openj9-0.27.0.tar.gz" ;;
-    17) JDK_URL="https://github.com/ibmruntimes/semeru17-binaries/releases/download/jdk-17.0.11%2B9_openj9-0.44.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_17.0.11_9_openj9-0.44.0.tar.gz" ;;
-    18) JDK_URL="https://github.com/ibmruntimes/semeru18-binaries/releases/download/jdk-18.0.2%2B9_openj9-0.33.1/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_18.0.2_9_openj9-0.33.1.tar.gz" ;;
-    19) JDK_URL="https://github.com/ibmruntimes/semeru19-binaries/releases/download/jdk-19.0.2%2B7_openj9-0.37.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_19.0.2_7_openj9-0.37.0.tar.gz" ;;
-    20) JDK_URL="https://github.com/ibmruntimes/semeru20-binaries/releases/download/jdk-20.0.2%2B9_openj9-0.40.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_20.0.2_9_openj9-0.40.0.tar.gz" ;;
-    21) JDK_URL="https://github.com/ibmruntimes/semeru21-binaries/releases/download/jdk-21.0.3%2B9_openj9-0.44.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_21.0.3_9_openj9-0.44.0.tar.gz" ;;
-    22) JDK_URL="https://github.com/ibmruntimes/semeru22-binaries/releases/download/jdk-22.0.1%2B8_openj9-0.45.0/ibm-semeru-open-jdk_${TRAVIS_CPU_ARCH}_linux_22.0.1_8_openj9-0.45.0.tar.gz" ;;
-    *) echo "JDK ${JAVA_VERSION} missing in the Semeru repository. Please choose a different version." ;;
-  esac
+ sudo apt remove --purge rabbitmq-server erlang*
+#  curl -1sLf 'https://dl.cloudsmith.io/public/rabbitmq/rabbitmq-erlang/setup.deb.sh' | sudo -E bash
+#  sudo apt update -y
+#  sudo apt install --upgrade erlang -y
+#  wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.13.3/rabbitmq-server_3.13.3-1_all.deb
+#  sudo dpkg -i rabbitmq-server_3.13.3-1_all.deb
+#  rm rabbitmq-server_3.13.3-1_all.deb
 
 
-echo $JDK_URL
+sudo apt-get update -y
 
-# Define the installation directory
-INSTALL_DIR="/usr/lib/jvm"
+sudo apt-get install curl gnupg apt-transport-https -y
 
-# Download the JDK tarball
-wget -q "$JDK_URL" -O jdk.tar.gz
+sudo apt-get install curl gnupg apt-transport-https -y
 
-# Extract the tarball
+## Team RabbitMQ's main signing key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null
+## Community mirror of Cloudsmith: modern Erlang repository
+curl -1sLf https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg > /dev/null
+## Community mirror of Cloudsmith: RabbitMQ repository
+curl -1sLf https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/rabbitmq.9F4587F226208342.gpg > /dev/null
 
-mkdir $INSTALL_DIR/jdk${JAVA_VERSION} && tar -xzf jdk.tar.gz --strip-components 1 -C "$INSTALL_DIR/jdk${JAVA_VERSION}"
+sudo tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
+## Provides modern Erlang/OTP releases from a Cloudsmith mirror
+##
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa1.novemberain.com/rabbitmq/rabbitmq-erlang/deb/ubuntu focal main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa1.novemberain.com/rabbitmq/rabbitmq-erlang/deb/ubuntu focal main
 
-# if [[ "${JAVA_VERSION}" == 8 ]]; then
-#   export JAVA_HOME="$INSTALL_DIR/jdk${JAVA_VERSION}"
-# else
-#   export JAVA_HOME="$INSTALL_DIR/jdk-${JAVA_VERSION}*"
-# fi
+# another mirror for redundancy
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa2.novemberain.com/rabbitmq/rabbitmq-erlang/deb/ubuntu focal main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.E495BB49CC4BBE5B.gpg] https://ppa2.novemberain.com/rabbitmq/rabbitmq-erlang/deb/ubuntu focal main
 
-export JAVA_HOME="$INSTALL_DIR/jdk${JAVA_VERSION}"
-export PATH=$JAVA_HOME/bin:$PATH
+## Provides RabbitMQ from a Cloudsmith mirror
+##
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa1.novemberain.com/rabbitmq/rabbitmq-server/deb/ubuntu focal main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa1.novemberain.com/rabbitmq/rabbitmq-server/deb/ubuntu focal main
 
-echo $JAVA_HOME
+# another mirror for redundancy
+deb [arch=amd64 signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa2.novemberain.com/rabbitmq/rabbitmq-server/deb/ubuntu focal main
+deb-src [signed-by=/usr/share/keyrings/rabbitmq.9F4587F226208342.gpg] https://ppa2.novemberain.com/rabbitmq/rabbitmq-server/deb/ubuntu focal main
+EOF
 
-# Reload the shell
-#source ~/.bashrc
 
-# Verify Java version
-java --version
+sudo apt-get update -y
+
+## Install Erlang packages
+sudo apt-get install -y erlang-base \
+                        erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+                        erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+                        erlang-runtime-tools erlang-snmp erlang-ssl \
+                        erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+
+## Install rabbitmq-server and its dependencies
+sudo apt-get install rabbitmq-server -y --fix-missing
