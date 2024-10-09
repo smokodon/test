c1a2e61fa41abe941f9749c82b295524abbf7b39
Konrad Wrzos <konrad@travis-ci.org>
2024-10-09 11:41:05 +0200

    ARM test

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-  arch=$(uname -m)
-  wget https://go.dev/dl/go1.21.1.linux-$arch.tar.gz
-  sudo tar -C /usr/local -xzf go1.21.1.linux-$arch.tar.gz
-  echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
-  export PATH=\$PATH:/usr/local/go/bin
-  sudo chown -R travis: /tmp/__common-lib.sh
-
-
+arch=$(uname -m)
+wget https://go.dev/dl/go1.21.1.linux-$arch.tar.gz
+sudo tar -C /usr/local -xzf go1.21.1.linux-$arch.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
+export PATH=\$PATH:/usr/local/go/bin
+sudo chown -R travis: /tmp/__common-lib.sh
