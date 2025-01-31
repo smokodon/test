e4fd61355b670257a52d79827dfb936560ee0ea7
Konrad Wrzos <konrad@travis-ci.org>
2025-01-31 14:38:20 +0100

    ANT tests

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-set -o errexit
-
-  local version="1.10.14"
-
   echo "Download and extract Ant"
-  wget -q "https://downloads.apache.org/ant/binaries/apache-ant-${version}-bin.tar.gz"
+  wget -q "https://downloads.apache.org/ant/binaries/apache-ant-1.10.14-bin.tar.gz"
   tar xzf apache-ant-$version-bin.tar.gz
   sudo mv apache-ant-$version/ /usr/local/ant
   echo "Add Ant variables to a profile"
